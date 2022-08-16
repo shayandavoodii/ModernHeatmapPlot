f9d91edd6da233d05eb1bfb685530d0ae5e8b2f4
Shayan Davoodi <52105833+shayandavoodii@users.noreply.github.com>
2022-08-16 22:56:19 +0430

    Update ModernHeatmap.py

--- ModernHeatmap.py
+++ ModernHeatmap.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-def ModernHeatmapPlot(data:pd.DataFrame, x_label:str, y_label:str, legend_label:str="Value", title=None, palette=None, save:bool=False):
+def ModernHeatmapPlot(data:pd.DataFrame, save:bool=False, **kwargs):
     """
     Draw a modern Heatmap plot utilizing Seaborn
 
@@ -13,24 +13,26 @@ def ModernHeatmapPlot(data:pd.DataFrame, x_label:str, y_label:str, legend_label:
         data:
             A 2-D pandas DataFrame with any shape
 
-        x_label:
-            The label of the x axis 
-
-        y_label:
-            The label of the y axis 
-
-        legend_label: default="Value"
-            Header of legend 
-
-        palette: default=None
-            A seaborn palette to change the colormap
-
-        title: String | default=None
-            Title of the plot
-
         save: default=False
             Save the fig in the current directory
 
+        kwargs:
+
+            x_label:
+                The label of the x axis 
+
+            y_label:
+                The label of the y axis 
+
+            legend_label: default="Value"
+                Header of legend 
+
+            title: String | default=None
+                Title of the plot
+
+            palette: default=None
+                A seaborn palette to change the colormap
+                
     
     Returns
 
@@ -38,19 +40,20 @@ def ModernHeatmapPlot(data:pd.DataFrame, x_label:str, y_label:str, legend_label:
     
     None
     """
-    data.index.name = 'Ticker'
-    df_long = data.reset_index().melt(id_vars='Ticker', var_name='Ticker2', value_name=legend_label)
+    options = {"x_label":"x tickers", "y_label":"y tickers", "legend_label":"Value", "title":None, "palette":sns.color_palette("coolwarm", as_cmap=True)}
+    if kwargs:
+        options |= kwargs
 
-    if not palette:
-        palette = sns.color_palette("coolwarm", as_cmap=True)
+    data.index.name = 'Ticker'
+    df_long = data.reset_index().melt(id_vars='Ticker', var_name='Ticker2', value_name=options["legend_label"])
 
     sns.set_style('darkgrid')
 
     g = sns.relplot(data=df_long,
                     x="Ticker2",
                     y="Ticker",
-                    size=legend_label,
-                    hue=legend_label,
+                    size=options["legend_label"],
+                    hue=options["legend_label"],
                     marker="s",
                     sizes=(20, 100),
                     palette=palette,
@@ -65,16 +68,16 @@ def ModernHeatmapPlot(data:pd.DataFrame, x_label:str, y_label:str, legend_label:
     
     # sns.move_legend(g, "lower center",bbox_to_anchor=(0.78, 0.5), title='Bin', frameon=False,)
     
-    g.set_xlabels(x_label)
-    g.set_ylabels(y_label)
+    g.set_xlabels(options["x_label"])
+    g.set_ylabels(options["y_label"])
     g.ax.set_facecolor('white')
     g.ax.grid(color='black', lw=0.45)
     g.fig.subplots_adjust(left=0.1, bottom=0.15)
     
-    if title:
-        plt.title(title)
+    if options["title"]:
+        plt.title(options["title"])
     
     if save:
-        g.savefig(f"fig {title}.jpeg", dpi=300)
+        g.savefig(f"fig {options['title']}.jpeg", dpi=300)
     
     plt.show()
